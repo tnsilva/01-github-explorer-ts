568808a330d00492be874510325aebd36ccea563
tnsilva <tnsilva@gmail.com>
2022-02-25 04:17:39 -0300

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { RepositoryList } from "./components/RepositoryList.jsx";
+import "./styles/global.scss";
+
+export function App() {
+  return (
+    <>
+      <RepositoryList />
+    </>
+  );
+}
